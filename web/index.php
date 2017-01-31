9e3b245aa306cf2227d21640a292d635545eb35b
Micha Alt <symfony@lean-stack.de>
2017-01-31 21:13:51 +0100

    Abstracted HTTP request and response via Symfony HttpFoundation.

--- web/index.php
+++ web/index.php
@@ -1,56 +1,31 @@
 <?php
 
+// Composer autoloader
+require '../vendor/autoload.php';
+
+// Request and Response abstraction
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
 $error = '';
 $posted = false;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($request->getMethod() === 'POST') {
 
-    if (isset($_POST['name']) && strlen($_POST['name']) > 0) {
+    $name = $request->get('name','');
+
+    if (strlen($name) > 0) {
         $posted = true;
-        $name = $_POST['name'];
     } else {
         $error = 'Ohne deinen Namen geht es hier nicht weiter ...';
     }
 }
 
-?>
-<!doctype html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+$template = file_get_contents('../app/Resources/views/index.html.php');
+$content = eval("?>$template");
 
-    <title>Symfony from Scratch</title>
-
-    <link rel="stylesheet" href="/css/pure.css">
-    <link rel="stylesheet" href="/css/medium-grid.css">
-    <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-    <div class="layout pure-g">
-        <div class="sidebar pure-u-1 pure-u-med-1-4">
-            <div class="header">
-                <h1>Symfony from Scratch</h1>
-            </div>
-        </div>
-        <div class="content pure-u-1 pure-u-med-3-4">
-            <div class="header">
-                <h2>Symfony sagt Hallo</h2>
-                <hr>
-            </div>
-            <section>
-            <?php if (!$posted): ?>
-                <form class="pure-form" method="post">
-                    <label for="name">Wer bist du?</label>
-                    <input id="name" name="name" type="text" placeholder="Dein Vorname" autofocus>
-                    <button type="submit" class="pure-button pure-button-primary">Sag es uns</button>
-                </form>
-                <?php if ($error): ?><div class="error"><?= $error ?></div><?php endif; ?>
-            <?php else: ?>
-                <div class="welcome">Herzlich Willkommen, <span class="break-on-sm">liebe/r <?= htmlentities($name) ?>!</span></div>
-            <?php endif; ?>
-            </section>
-        </div>
-    </div>
-</body>
-</html>
+$response = new Response($content);
+echo $response;
