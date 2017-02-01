edc2f0aa5a63420ed2a9eed37a0220074b32c9bd
Micha Alt <mail@lean-stack.de>
2017-02-01 13:06:33 +0100

    Let the HttpKernel handle the request.

--- web/index.php
+++ web/index.php
@@ -3,9 +3,21 @@
 // Composer autoloader
 require '../vendor/autoload.php';
 
+// Enable debugging infos
+\Symfony\Component\Debug\Debug::enable();
+
+// Create a plain HttpKernel
+$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
+$resolver = new \Symfony\Component\HttpKernel\Controller\ControllerResolver();
+$kernel = new \Symfony\Component\HttpKernel\HttpKernel($dispatcher,$resolver);
+
 $request =  \Symfony\Component\HttpFoundation\Request::createFromGlobals();
 
+// as of no router present, i'm setting the controller manually
 $ctrl = new \AppBundle\Controller\DefaultController();
-$response = $ctrl->indexAction($request);
+$request->attributes->add(['_controller' => [$ctrl,'indexAction']]);
 
+$response = $kernel->handle($request);
 $response->send();
+
+$kernel->terminate($request,$response);
