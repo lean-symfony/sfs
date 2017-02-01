41f7ab291f09aa1abd2ff25a2f47762f28feecb5
Micha Alt <symfony@lean-stack.de>
2017-02-01 22:20:27 +0100

    Refactoring: moved the http_kernel service definition to the FrameworkBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,14 +8,16 @@ class AppKernel extends \Symfony\Component\HttpKernel\Kernel
     /**
      * Returns an array of bundles to register.
      *
-     * @return \Symfony\Component\HttpKernel\Bundle\BundleInterface An array of bundle instances
+     * @return \Symfony\Component\HttpKernel\Bundle\BundleInterface[] An array of bundle instances
      */
     public function registerBundles()
     {
-        return [
+        $bundles = [
             new \LeanSymfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \AppBundle\AppBundle()
         ];
+
+        return $bundles;
     }
 
     /**
@@ -36,36 +38,6 @@ class AppKernel extends \Symfony\Component\HttpKernel\Kernel
      */
     public function registerContainerConfiguration(\Symfony\Component\Config\Loader\LoaderInterface $loader)
     {
-        $loader->load(function (\Symfony\Component\DependencyInjection\ContainerBuilder $c) use ($loader) {
-
-            $c->setDefinition('app.logger.console_handler',
-                new Definition(\Monolog\Handler\BrowserConsoleHandler::class));
-
-            $loggerDefinition = new Definition(\Monolog\Logger::class, ['Lean Symfony']);
-            $loggerDefinition->addMethodCall('pushHandler', [new Reference('app.logger.console_handler')]);
-            $c->setDefinition('app.kernel.logger', $loggerDefinition);
-
-            $c->setDefinition('app.event_dispatcher',
-                new Definition(\LeanSymfony\Component\EventDispatcher\EventDispatcher::class, [
-                    new Reference('app.kernel.logger')
-                ])
-            );
-
-            $c->setDefinition('app.controller_resolver',
-                new Definition(\Symfony\Component\HttpKernel\Controller\ControllerResolver::class, [
-                    new Reference('app.kernel.logger')
-                ])
-            );
-
-            $c->setDefinition('http_kernel',
-                new Definition(\Symfony\Component\HttpKernel\HttpKernel::class, [
-                    new Reference('app.event_dispatcher'),
-                    new Reference('app.controller_resolver')
-                ])
-            );
-
-            $c->addObjectResource($this);
-        });
     }
 
     public function getCacheDir()
