45331146bb971e339d138163a3dfc061789452d1
Micha Alt <symfony@lean-stack.de>
2017-02-01 21:13:26 +0100

    Refactoring: building service definitions in the kernel.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,5 +1,8 @@
 <?php
 
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
 class AppKernel extends \Symfony\Component\HttpKernel\Kernel
 {
     /**
@@ -19,7 +22,36 @@ class AppKernel extends \Symfony\Component\HttpKernel\Kernel
      */
     public function registerContainerConfiguration(\Symfony\Component\Config\Loader\LoaderInterface $loader)
     {
+        $loader->load(function (\Symfony\Component\DependencyInjection\ContainerBuilder $c) use ($loader) {
 
+            $c->setDefinition('app.logger.console_handler',
+                new Definition(\Monolog\Handler\BrowserConsoleHandler::class));
+
+            $loggerDefinition = new Definition(\Monolog\Logger::class, ['Lean Symfony']);
+            $loggerDefinition->addMethodCall('pushHandler', [new Reference('app.logger.console_handler')]);
+            $c->setDefinition('app.logger', $loggerDefinition);
+
+            $c->setDefinition('app.event_dispatcher',
+                new Definition(\LeanSymfony\Component\EventDispatcher\EventDispatcher::class, [
+                    new Reference('app.logger')
+                ])
+            );
+
+            $c->setDefinition('app.controller_resolver',
+                new Definition(\Symfony\Component\HttpKernel\Controller\ControllerResolver::class, [
+                    new Reference('app.logger')
+                ])
+            );
+
+            $c->setDefinition('http_kernel',
+                new Definition(\Symfony\Component\HttpKernel\HttpKernel::class, [
+                    new Reference('app.event_dispatcher'),
+                    new Reference('app.controller_resolver')
+                ])
+            );
+
+            $c->addObjectResource($this);
+        });
     }
 
     public function getCacheDir()
--- web/index.php
+++ web/index.php
@@ -6,19 +6,8 @@ require '../vendor/autoload.php';
 // Enable debugging infos
 \Symfony\Component\Debug\Debug::enable();
 
-// Create a logger
-$log = new \Monolog\Logger('Lean Symfony');
-$log->pushHandler(new \Monolog\Handler\BrowserConsoleHandler());
-
-// Create a plain HttpKernel
-$dispatcher = new \LeanSymfony\Component\EventDispatcher\EventDispatcher($log);
-$resolver = new \Symfony\Component\HttpKernel\Controller\ControllerResolver($log);
-$httpKernel = new \Symfony\Component\HttpKernel\HttpKernel($dispatcher,$resolver);
-
 // And the Application Kernel with a registered http_kernel service
 $kernel = new AppKernel('dev', true);
-$kernel->boot();    // build the container
-$kernel->getContainer()->set('http_kernel', $httpKernel);
 
 $request =  \Symfony\Component\HttpFoundation\Request::createFromGlobals();
 
