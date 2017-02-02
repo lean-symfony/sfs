618478fc90266c5e5169bc0d54b2b6399df0c338
Micha Alt <symfony@lean-stack.de>
2017-02-02 19:56:02 +0100

    Routing: basic routing configured.

--- src/LeanSymfony/Bundle/FrameworkBundle/FrameworkBundle.php
+++ src/LeanSymfony/Bundle/FrameworkBundle/FrameworkBundle.php
@@ -2,9 +2,27 @@
 
 namespace LeanSymfony\Bundle\FrameworkBundle;
 
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FrameworkBundle extends Bundle
 {
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * This method can be overridden to register compilation passes,
+     * other extensions, ...
+     *
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new RegisterListenersPass('lean.event_dispatcher'), PassConfig::TYPE_BEFORE_REMOVING);
+    }
 
 }
--- src/LeanSymfony/Component/EventDispatcher/EventDispatcher.php
+++ src/LeanSymfony/Component/EventDispatcher/EventDispatcher.php
@@ -3,10 +3,11 @@
 namespace LeanSymfony\Component\EventDispatcher;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\Event;
-use Symfony\Component\EventDispatcher\EventDispatcher as BaseEventDispatcher;
+use Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher;
 
-class EventDispatcher extends BaseEventDispatcher
+class EventDispatcher extends ContainerAwareEventDispatcher
 {
     /**
      * @var LoggerInterface
@@ -15,10 +16,13 @@ class EventDispatcher extends BaseEventDispatcher
 
     /**
      * EventDispatcher constructor.
+     * @param ContainerInterface $container
      * @param LoggerInterface $log
      */
-    public function __construct(LoggerInterface $log)
+    public function __construct(ContainerInterface $container, LoggerInterface $log)
     {
+        parent::__construct($container);
+
         $this->log = $log;
     }
 
